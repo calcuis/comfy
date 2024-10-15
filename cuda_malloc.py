a62db15ab301a8503fd70e903edeed487276ed51
calcuis <113646141+calcuis@users.noreply.github.com>
2024-10-15 13:39:50 -0800

    Update cuda_malloc.py

--- cuda_malloc.py
+++ cuda_malloc.py
@@ -1,7 +1,6 @@
-import os
+import os, subprocess
 import importlib.util
 from comfy.cli_args import args
-import subprocess
 
 #Can't use pytorch to get the GPU names because the cuda malloc has to be set before the first import.
 def get_gpu_names():
