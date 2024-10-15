c630a2dd50e3ea5ebd5663a1d762196aafc2cf2f
calcuis <113646141+calcuis@users.noreply.github.com>
2024-10-15 13:33:10 -0800

    Update basic_api_example.py

--- script_examples/basic_api_example.py
+++ script_examples/basic_api_example.py
@@ -104,7 +104,6 @@ def queue_prompt(prompt):
     req =  request.Request("http://127.0.0.1:8188/prompt", data=data)
     request.urlopen(req)
 
-
 prompt = json.loads(prompt_text)
 #set the text prompt for our positive CLIPTextEncode
 prompt["6"]["inputs"]["text"] = "masterpiece best quality man"
@@ -112,7 +111,4 @@ prompt["6"]["inputs"]["text"] = "masterpiece best quality man"
 #set the seed for our KSampler node
 prompt["3"]["inputs"]["seed"] = 5
 
-
 queue_prompt(prompt)
-
-
